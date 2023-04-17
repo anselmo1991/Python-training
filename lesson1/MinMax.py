2223b5bb5b83624ae872cf71fe4610e392c9d371
anselmo1991 <freulein.sonya@gmail.com>
2023-04-17 10:15:02 +0200

    list filtering

--- lesson1/MinMax.py
+++ lesson1/MinMax.py
@@ -4,6 +4,22 @@ new_numbers = []
 for val in numbers:
 
     try:
+        if isinstance(val, int) == True:
+            new_numbers.append(val)
+    except (TypeError, ValueError):
+        pass
+
+print(new_numbers)
+print('Min value', min(new_numbers))
+print('Max value', max(new_numbers))
+
+
+numbers = [1, 2, '0', '300', -2.5, 'Dog', True, 0o1256, None]
+new_numbers = []
+
+for val in numbers:
+
+    try:
         new_numbers.append(int(val))
     except (TypeError, ValueError):
         pass
