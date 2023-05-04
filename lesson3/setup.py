de375b080304f556d88ba98cef61e34f253b72f5
anselmo1991 <freulein.sonya@gmail.com>
2023-05-04 10:44:01 +0200

    ls-lh

---
+++ lesson3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='Python training',
+    version='0.1',
+    description='This package consists of tasks completed as part of the Python course',
+    url='https://github.com/anselmo1991/Python-training',
+    author='Sofia Shilova',
+    author_email='freulein.sonya@gmail.com',
+    license='free',
+    packages=['C:/Users/Sofia_Shilova/Python-training'],
+    install_requires=[
+        'win32security',
+        'prettytable',
+        'pathlib',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
