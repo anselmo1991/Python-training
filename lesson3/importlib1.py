8e297112c79f0d4f255de364b6ac06bdbdc22084
anselmo1991 <freulein.sonya@gmail.com>
2024-01-15 09:06:05 +0100

    lesson3

--- lesson3/importlib1.py
+++ lesson3/importlib1.py
@@ -1,11 +1,8 @@
 import importlib
+import pathlib
 
-def get_package_path(setup):
-    loader = importlib.find_loader(setup)
-    if loader is None:
+def get_package_path(package_name):
+    try:
+        return str(pathlib.Path(importlib.import_module(package_name).__file__).parent)
+    except ModuleNotFoundError:
         return "Package not found"
-    else:
-        return loader.path
-
-
-print(get_package_path('setup'))
