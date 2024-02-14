149ff983a6024eaefcb6809fbe8eaf46ac623f40
anselmo1991 <freulein.sonya@gmail.com>
2024-02-14 14:57:02 +0100

    lesson3

--- lesson2/ip_validation.py
+++ lesson2/ip_validation.py
@@ -14,6 +14,12 @@ def validate_ip_address(ip_address):
 
 print(validate_ip_address("176.132.0.1"))
 print(validate_ip_address("45.4.0.256"))
+print(validate_ip_address(""))
+print(validate_ip_address("192.168.0.1"))
+print(validate_ip_address("0.0.0.1"))
+print(validate_ip_address("10.100.500.32"))
+print(validate_ip_address("700"))
+print(validate_ip_address("127.0.1"))
 
 # with socket.inet_aton
 import socket
@@ -27,3 +33,9 @@ def validate_ip_address(ip_address):
 
 print(validate_ip_address("176.132.0.1"))
 print(validate_ip_address("45.4.0.256"))
+print(validate_ip_address(""))
+print(validate_ip_address("192.168.0.1"))
+print(validate_ip_address("0.0.0.1"))
+print(validate_ip_address("10.100.500.32"))
+print(validate_ip_address("700"))
+print(validate_ip_address("127.0.1"))
